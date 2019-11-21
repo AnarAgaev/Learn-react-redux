af0468af48d37e19ae8ddac73d18ed803f8acf6d
Anar Agaev <anar.n.agaev@gmail.com>
2019-11-21 15:00:43 +0300

    created exampel of react component

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,32 +1,50 @@
 /**
- * React элементы
- *  const element = <h1>Hi</h1>; - пример реакт элемента
+ * React компоненты - пользовательские интерфейсы состоят из компонентов
+ * Просто создать React компонент - Нужно создать функцию которая возвращает React элемента
+ * После создани можно использовать компонент в любом мест обернув его в тег <ИМЯ_КОМПОНЕНТА/>
  *
- * React элементы создаются при помощи JSX синтаксиса, но могут быть и созданы по средствам нативного React
- *  на натисном React: const el = React.createElement("h1", null, 'Hello World!!!!');
- * Легковесные объекты - Virtual DOM
- * ReactDOM.render превращает React элесенты в обычные браузерные DOM элементы и рендерит их на странице
+ * !!!ОБЯЗАТЕЛЬНО имя компонента должно начинаться с большой буквы
  *
- * При создании любого реакт документа/скрипта/файла:
- * 1. Ипортируем библеотеки React и ReactDOM
- * 2. Создаём react элементы в JSX синтаксисе
- * 3. Рендерим/отрисовываем react элементы на странице
+ * Пример создания компонента:
+ * const Component_Name = () => {
+ *   return <tag>Some component content<tag/>
+ * };
  *
+ * React компоненты - это функции, которые возвращают РЕ элемент
+ * Должны начинаться с большой буквы
+ * Имя затем можно использовать в JSX как будто это HTML элемент
  *
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input type="text" placeholder="search"/>
+// Примеры создания компонента
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome app</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+const SearchPanel = () => {
+  return <input type="text" placeholder="search"/>;
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
