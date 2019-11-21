b2d813c26499607c147a6430a9c358544f34335a
Anar Agaev <anar.n.agaev@gmail.com>
2019-11-21 15:52:38 +0300

    first lessom by JSX

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,45 +1,68 @@
 /**
- * React компоненты - пользовательские интерфейсы состоят из компонентов
- * Просто создать React компонент - Нужно создать функцию которая возвращает React элемента
- * После создани можно использовать компонент в любом мест обернув его в тег <ИМЯ_КОМПОНЕНТА/>
+ * JSC
  *
- * !!!ОБЯЗАТЕЛЬНО имя компонента должно начинаться с большой буквы
+ * JSC похож на HTML
+ * в JSC можно использовать как обычные HTML теги, так и имена React компонентов
+ * если JSC код занимает несколько строк, то его нужно обернуть в круглые скобки
+ * блок JSC кода создаёт React элемент, поэтому корневым элементом может быть только один элемнет
+ * JSC может использовать внутти себя JS выражения, для этого их нужно обернутть в фигурные скоби {JS code}
  *
- * Пример создания компонента:
- * const Component_Name = () => {
- *   return <tag>Some component content<tag/>
- * };
  *
- * React компоненты - это функции, которые возвращают РЕ элемент
- * Должны начинаться с большой буквы
- * Имя затем можно использовать в JSX как будто это HTML элемент
+ * Позволяет использовать выражения {foo.bar}
+ * Атрибуты нзываются camelCase'ом
+ * class = className, for = htmlFor !Исключения
+ * В свойства можно передавать любое значение
+ * null, undefined, true и  false в теле тегов игнорируются (не вызывая ошибки)
  *
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Примеры создания компонента
 const TodoList = () => {
+  const items = ['Drink coffee', 'Build Awesome app'];
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Awesome app</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
   );
 };
 
 const SearchPanel = () => {
-  return <input type="text" placeholder="search"/>;
+  const searchText = 'Type here to search';
+  return <input type="text"
+
+    className=""   //*** !Исключение в JSX атрибут class Должен называться className
+    htmlFor=""     //*** !Исключение в JSX атрибут for должен называться htmlFor
+                   //*** !Остальные элементы называются также как и в Html, но в camelCase
+
+    placeholder={ searchText }
+    autoComplete=""
+    tabIndex=""/>;
 };
 
 const AppHeader = () => {
-  return <h1>My Todo List</h1>;
+  const titleText = 'My Todo List';
+  const headerStyle = {
+    fontSize: '37px',
+    color: 'red'
+  };
+  return <h1 style={ headerStyle } >{ titleText }</h1>;
 };
 
 const App = () => {
+  const isLoggedIn = false;
+  const loginBox = <span>Log in plase<br/></span>;
+  const welcomeBox = <span>Welcome Back<br/></span>;
+
+  const value = '<script>alert("");</script>';
+
   return (
     <div>
+      { value }
+      { isLoggedIn ? welcomeBox : loginBox }
+      <span>{ (new Date()).toString() }</span>
       <AppHeader/>
       <SearchPanel/>
       <TodoList/>
