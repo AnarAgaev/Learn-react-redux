b993119e22b1fc1f9dbda0a18fd81f78dcf4d3bb
Anar Agaev <anar.n.agaev@gmail.com>
2019-11-21 16:16:28 +0300

    test code review

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,71 +1,34 @@
 /**
- * JSC
+ * Задание 1: найти ошибки в коде ниже и написать правильный код
  *
- * JSC похож на HTML
- * в JSC можно использовать как обычные HTML теги, так и имена React компонентов
- * если JSC код занимает несколько строк, то его нужно обернуть в круглые скобки
- * блок JSC кода создаёт React элемент, поэтому корневым элементом может быть только один элемнет
- * JSC может использовать внутти себя JS выражения, для этого их нужно обернутть в фигурные скоби {JS code}
+ * 1. import ReactDOM from 'react-dom';
+ * 2.
+ * 3. const loginBox = () => {
+ * 4.   return <span class="login">Login Box</span>
+ * 5. };
+ * 6.
+ * 7. const App = () => {
+ * 8.  return (
+ * 9.    <div>Header</div>
+ * 10.    <loginBox/>
+ * 11.  );
+ * 12. };
  *
- *
- * Позволяет использовать выражения {foo.bar}
- * Атрибуты нзываются camelCase'ом
- * class = className, for = htmlFor !Исключения
- * В свойства можно передавать любое значение
- * null, undefined, true и  false в теле тегов игнорируются (не вызывая ошибки)
- *
+ * ReactDOM.render(App);
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-  const items = ['Drink coffee', 'Build Awesome app'];
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-    </ul>
-  );
-};
-
-const SearchPanel = () => {
-  const searchText = 'Type here to search';
-  return <input type="text"
-
-    className=""   //*** !Исключение в JSX атрибут class Должен называться className
-    htmlFor=""     //*** !Исключение в JSX атрибут for должен называться htmlFor
-                   //*** !Остальные элементы называются также как и в Html, но в camelCase
-
-    placeholder={ searchText }
-    autoComplete=""
-    tabIndex=""/>;
-};
-
-const AppHeader = () => {
-  const titleText = 'My Todo List';
-  const headerStyle = {
-    fontSize: '37px',
-    color: 'red'
-  };
-  return <h1 style={ headerStyle } >{ titleText }</h1>;
+const LoginBox = () => {
+  return <span className="login">Login Box</span>
 };
 
 const App = () => {
-  const isLoggedIn = false;
-  const loginBox = <span>Log in plase<br/></span>;
-  const welcomeBox = <span>Welcome Back<br/></span>;
-
-  const value = '<script>alert("");</script>';
-
   return (
     <div>
-      { value }
-      { isLoggedIn ? welcomeBox : loginBox }
-      <span>{ (new Date()).toString() }</span>
-      <AppHeader/>
-      <SearchPanel/>
-      <TodoList/>
+      <div>Header</div>
+      <LoginBox/>
     </div>
   );
 };
