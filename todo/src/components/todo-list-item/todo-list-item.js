e4254a84c6159b17ea5ac752141276c64b18da9c
Anar Agaev <anar.n.agaev@gmail.com>
2019-11-23 16:57:58 +0300

    setState() - как он работает

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,10 +1,7 @@
 /**
- * State - Состояния компоента
+ * Как работает setState()
+ * В setState() нужно передавать только измения в state
  *
- * Состояния хранятся в поле state у Компонента класса
- * this.state инициализируктся в конструкторе или в теле класса
- * После инициализации state нельзя изменить (тлько читать)
- * Чтобы обносить state используем метод setState()
  *
  */
 
@@ -14,7 +11,8 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component {
 
   state = {
-    done: false
+    done: false,
+    important: false
   };
 
   onLabelClick = () => {
@@ -23,31 +21,37 @@ export default class TodoListItem extends Component {
     });
   };
 
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
+
   render () {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
+
     if (done) {
       classNames += ' done';
     }
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += ' important';
+    }
 
     return (
       <span className={ classNames }>
         <span
           className="todo-list-item-label"
-          style={ style }
           onClick={ this.onLabelClick } >
           { label }
         </span>
 
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={ this.onMarkImportant } >
           <i className="fa fa-exclamation" />
         </button>
 
