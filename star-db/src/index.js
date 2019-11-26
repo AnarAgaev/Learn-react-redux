af0789971736e0d6fb68bd0c20cb5977e3d3e47a
Anar Agaev <anar.n.agaev@gmail.com>
2019-11-26 16:09:24 +0300

    Fetch API как работает fetch запрос к серверу

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+/**
+ * http://swapi.co
+ *
+ * Как работет Fetch API
+ *
+ * Чтобы получить данные с сервера, нужно выполнить два вызова (каждый вернет Promise):
+ *  res = await fetch(url);
+ *  body = await res.json();
+ *
+ *  Кроме .json() есть другие функции для других типов ответа: arrayBuffer(), blob(), text(), formData()
+ *
+ */
+
+const getResource = async (url) => {
+  const response = await fetch(url);
+  const body = await response.json();
+  return body;
+};
+
+getResource('https://swapi.co/api/people/4/')
+  .then((body) => {
+    console.log(body);
+  });
+
+// fetch('https://swapi.co/api/people/1/')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((body) => {
+//     console.log(body);
+//   });
