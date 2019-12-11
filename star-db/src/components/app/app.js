4de28076097ff15ed8b8ac6ff9ed3bc003f01d63
Anar Agaev <anar.n.agaev@gmail.com>
2019-12-11 15:06:22 +0300

    Основы React Router

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -1,9 +1,17 @@
 /**
- * Routing
+ * Основы React Router
  *
- * Роутинг - переключение междоу виртуальными "страницами" UI приложения
- * Роутинг нужен, чтобы упростить структуру приложения и организовать навигацию
- * В Single Page Application страница, на самом деле, одна и она не перезагружается
+ * Первое, что нужно сделать это установить библеотеку React Router - npm install react-router-dom
+ *
+ * Пример роутинга для приложения:
+ * <Router>
+ *    <Route path="/blog" component={Blog} />
+ *    <Route path="/about" component={About} />
+ *    <Route path="/shop" component={Shop} />
+ * </Router>
+ *
+ * React Router это не часть React. Есть и другие библиотеки для роутинга (к примеру, UI-Router)
+ *
  *
  */
 
@@ -15,6 +23,9 @@ import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 import { SwapiServiceProvider } from '../swapi-service-context';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './app.css';
 
 export default class App extends Component {
@@ -38,13 +49,15 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app container">
-            <Header onServiceChange={this.onServiceChange} />
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
-          </div>
+          <Router>
+            <div className="stardb-app container">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
+              <Route path="/people" component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" component={StarshipsPage} />
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
