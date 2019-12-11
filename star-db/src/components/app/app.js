504cec870311fa64b4c8c5bdc01067879a530e2a
Anar Agaev <anar.n.agaev@gmail.com>
2019-12-11 15:48:48 +0300

    Как работает Rout

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -1,5 +1,9 @@
 /**
- * Link - переход по навигационным ссылкам
+ * Как работает Rout
+ *
+ * В Route можно передать render функцию: <Route path="/hi" render={() => <p>Hi</p>} />
+ * Route работает как фильтр - сравнивая path с текущим адресом он решает, отрисовать содержимое или нет.
+ * Параметр exact говорит, что нужно использовать точное совпадение (а не "path является частью адреса")
  *
  */
 
@@ -11,9 +15,7 @@ import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 import { SwapiServiceProvider } from '../swapi-service-context';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import './app.css';
 
 export default class App extends Component {
@@ -41,6 +43,10 @@ export default class App extends Component {
             <div className="stardb-app container">
               <Header onServiceChange={this.onServiceChange} />
               <RandomPlanet />
+
+              <Route path="/"
+                     render={() => <h2>Welcome to StarDB</h2>}
+                     exact={true} />
               <Route path="/people" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
               <Route path="/starships" component={StarshipsPage} />
