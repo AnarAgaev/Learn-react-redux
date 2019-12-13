79e98b77d9854b6964e1bcf3e85de525faa28ae5
Anar Agaev <anar.n.agaev@gmail.com>
2019-12-13 14:49:53 +0300

    'Reducer'

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,8 +1,23 @@
 /*
-* Создаём новый проект
+* Reducer
 *
-* Для работы с Redux понадобятся две бибилиотеки:
-* redux - сам Redux (npm i redux)
-* react-redux - библиотека, которая уарощает интеграцию между React и Redux (npm i react-redux)
+* Reducer это обычная функция:
+* (state, action) => newState
+* Если state - undefined, то нужно вернуть первоначальный (initial) state
+* Если тип action неизвестен - нужно вернуть state без изменений
 *
 */
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+state = reducer(state, {type: 'INC'});
+
+console.log(state)
